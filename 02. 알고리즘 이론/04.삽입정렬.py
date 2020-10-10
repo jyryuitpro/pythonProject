6bd03c426c03d1f190ca6144aa8dbcc2b8695fb7
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-10 20:58:12 +0900

    04. 삽입 정렬

---
+++ 02. 알고리즘 이론/04.삽입정렬.py
@@ -0,0 +1,13 @@
+import random
+
+def insertion_sort(data_list):
+    for turn in range(len(data_list) - 1):
+        for check in range(turn + 1, 0, -1):
+            if data_list[check] < data_list[check - 1]:
+                data_list[check], data_list[check - 1] = data_list[check - 1], data_list[check]
+            else:
+                break
+    return data_list
+
+data_list = random.sample(range(100), 50)
+print (insertion_sort(data_list))
