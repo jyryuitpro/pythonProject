c950706f78715ce1ba321122c597701d12a0e1e0
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-10 11:36:31 +0900

    11. 링크드 리스트 (Linked List) -1

---
+++ 01. 자료구조 이론/11.링크드리스트(Linked List)-1.py
@@ -0,0 +1,31 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def add(data):
+    node = head
+    while node.next:
+        node = node.next
+    node.next = Node(data)
+
+node1 = Node(1)
+head = node1
+
+for index in range(1, 10):
+    add(index)
+
+node = head
+while node.next:
+    print(node.data)
+    node = node.next
+print(node.data)
+
+
+
+
