d5ada48d94ce642dfd25e6428baa4a6be8dca181
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-15 15:04:00 +0900

    18. 해쉬 테이블 - 해쉬 테이블 - 2

---
+++ 01. 자료구조 이론/18.해쉬테이블-해쉬테이블-2.py
@@ -0,0 +1,23 @@
+hash_table = list([0 for i in range(8)])
+
+# 해쉬 키 생성
+def get_key(data):
+    return hash(data)
+
+# 해쉬 함수
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    hash_address = hash_function(get_key(data))
+    hash_table[hash_address] = value
+
+def read_data(data):
+    hash_address = hash_function(get_key(data))
+    return hash_table[hash_address]
+
+save_data("Dave", "01020302000")
+save_data("Andy", "01033232200")
+
+print(read_data("Dave"))
+print(hash_table)
