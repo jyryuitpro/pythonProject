870fc0753ad0fbf1afe5944038b89f32b07f3eb9
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-10-17 11:17:02 +0900

    21. 해쉬 테이블 - 해쉬 테이블 - 5

---
+++ 01. 자료구조 이론/21.해쉬테이블-해쉬테이블-5.py
@@ -0,0 +1,44 @@
+import hashlib
+
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+    hash_object = hashlib.sha256()
+    hash_object.update(data.encode())
+    hex_dig = hash_object.hexdigest()
+    return int(hex_dig, 16)
+
+def hash_function(key):
+    return key % 8
+
+def save_data(data, value):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                hash_table[index] = [index_key, value]
+                return
+            elif hash_table[index][0] == index_key:
+                hash_table[index][1] = value
+                return
+    else:
+        hash_table[hash_address] = [index_key, value]
+
+
+def read_data(data):
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+
+    if hash_table[hash_address] != 0:
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index] == 0:
+                return None
+            elif hash_table[index][0] == index_key:
+                return hash_table[index][1]
+    else:
+        return None
+
+save_data('da', '01200123123')
+save_data('dh', '3333333333')
+read_data('dh')
