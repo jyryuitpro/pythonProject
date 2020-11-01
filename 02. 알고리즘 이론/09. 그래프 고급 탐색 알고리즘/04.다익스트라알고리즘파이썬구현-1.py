2c90e8aaf52d5690b7d3ba9ae1ecf3d0749b9c5a
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-01 14:58:45 +0900

    04. 다익스트라 알고리즘 파이썬 구현 - 2

--- 02. 알고리즘 이론/09. 그래프 고급 탐색 알고리즘/04.다익스트라알고리즘파이썬구현-1.py
+++ 02. 알고리즘 이론/09. 그래프 고급 탐색 알고리즘/04.다익스트라알고리즘파이썬구현-1.py
@@ -21,9 +21,25 @@ mygraph = {
     'F':{'A':5}
 }
 
-def dijksta(graph, start):
+def dijkstra(graph, start):
     distances = {node: float('inf') for node in graph}
     distances[start] = 0
     queue = []
+    heapq.heappush(queue, [distances[start], start])
 
-    heapq.heappush(queue, distances[start], start)
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if distances[current_node] < current_distance:
+            continue
+
+        for adjacent, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[adjacent]:
+                distances[adjacent] = distance
+                heapq.heappush(queue, [distance, adjacent])
+
+    return distances
+
+print(dijkstra(mygraph, 'A'))
