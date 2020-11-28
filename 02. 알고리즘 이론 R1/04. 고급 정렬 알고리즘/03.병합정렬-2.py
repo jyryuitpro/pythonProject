bfe95d225dcbfc107e29fc0249f0501e7a010427
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-28 23:54:37 +0900

    03. 병합 정렬 - 2

---
+++ 02. 알고리즘 이론 R1/04. 고급 정렬 알고리즘/03.병합정렬-2.py
@@ -0,0 +1,18 @@
+def split(data):
+    medium = int(len(data) / 2)
+    left = data[:medium]
+    right = data[medium:]
+    print(left, right)
+
+data_list = [3, 4, 1, 3, 2]
+split(data_list)
+
+def mergesplit(data):
+    if len(data) <= 1:
+        return data
+
+    medium = int(len(data) / 2)
+    left = mergesplit(data[:medium])
+    right = mergesplit(data[medium:])
+
+    return merge(left, right)
