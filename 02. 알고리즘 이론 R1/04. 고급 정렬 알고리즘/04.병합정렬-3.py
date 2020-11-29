4889d2dde209fb4f14aa12a1cf71669698314973
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-29 22:51:24 +0900

    04. 병합 정렬 - 3

---
+++ 02. 알고리즘 이론 R1/04. 고급 정렬 알고리즘/04.병합정렬-3.py
@@ -0,0 +1,21 @@
+def merge(left, right):
+    merged = list()
+    left_point, right_point = 0, 0
+
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+
+    return merged
